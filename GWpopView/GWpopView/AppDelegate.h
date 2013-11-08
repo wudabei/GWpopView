6600d615bc8105aa54b0952dcc5b1429a730b49b
gary <wudabei@gmail.com>
2013-11-08 12:39:42 +0800

    //test

---
+++ GWpopView/GWpopView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GWpopView
+//
+//  Created by cannaan on 13-11-8.
+//  Copyright (c) 2013年 cannaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GWpopView/GWpopView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GWpopView
+//
+//  Created by cannaan on 13-11-8.
+//  Copyright (c) 2013年 cannaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
